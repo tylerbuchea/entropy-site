7112bf2e9e7286c5cdfe14626de8956c8ed699b4
Tyler Buchea <tyler@buchea.com>
2017-07-31 17:33:21 -0700

    Get ratios for soon to be updated iprep script

--- iprep.sh
+++ iprep.sh
@@ -4,6 +4,13 @@
 # 367x429
 # 488x235
 
+# 1: 12 / 01 / 24 / 05 : 07 : (4 / 12)
+# 2: 14 / 05 / 28 / 10 : 11 : (5 / 14)
+# 3: 01 / 10 / 26 / 15 : 15 : (5 / 25)
+# 4: 11 / 15 / 29 / 24 : 17 : (9 / 18)
+# 5: 01 / 15 / 11 / 27 : 21 : (12 / 26)
+# 6: 11 / 24 / 20	/ 30 : 06 : (6 / 19)
+
 # This is for generating cropping out standard sizes to go into the site grid
 
 echo "Enter image location: "
