c2f9427c6becf1231a465a1b620a1b10e754f1d2
Tyler Buchea <tyler@buchea.com>
2017-08-20 19:49:34 -0700

    Update theme color

--- src/index.js
+++ src/index.js
@@ -162,5 +162,26 @@ function modalClose() {
   }, 500);
 }
 
+(function () {
+  var throttle = function (type, name, obj) {
+    obj = obj || window;
+    var running = false;
+    var func = function () {
+      if (running) {
+        return;
+      }
+      running = true;
+      requestAnimationFrame(function () {
+        obj.dispatchEvent(new CustomEvent(name));
+        running = false;
+      });
+    };
+    obj.addEventListener(type, func);
+  };
+  throttle('resize', 'optimizedResize');
+})();
+
+window.addEventListener('optimizedResize', modalClose);
+
 setState(initialState);
 blurUp(initialState);
