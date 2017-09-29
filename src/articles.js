60dc288af735d5ba3eea8996c104291fa5431fa6
Tyler Buchea <tyler@buchea.com>
2017-09-29 06:47:49 -0700

    Add container to articles for width restriction

--- src/articles.js
+++ src/articles.js
@@ -5,38 +5,42 @@ const articles = {
     </div>
 
     <div class="modal-article">
+      <div class="container">
 
-      <a class="button" target="_blank" href="https://dr5en.surge.sh">PLAY NOW!</a>
+        <div style="text-align:center;margin:20px auto 30px auto;">
+          <a class="button" target="_blank" href="https://dr5en.surge.sh">PLAY NOW!</a>
+        </div>
 
-      <p>
-        This was a project I worked on with another Portland agency
-        for a high profile client. It is a racing game that involves
-        drones and was used as part of a training program for partnered
-        businesses to learn more about the client and their offerings.
-      </p>
+        <p>
+          This was a project I worked on with another Portland agency
+          for a high profile client. It is a racing game that involves
+          drones and was used as part of a training program for partnered
+          businesses to learn more about the client and their offerings.
+        </p>
 
-      <!-- <iframe src="https://starmaster.surge.sh" /> -->
-      <img src="http://lorempixel.com/400/200?fuck=off" />
+        <!-- <iframe src="https://starmaster.surge.sh" /> -->
+        <img src="http://lorempixel.com/400/200?fuck=off" />
 
-      <p>
-        This project was created right before web browsers
-        were upgraded with some very specific technologies that
-        would allow good performance on mobile browsers. The initial
-        version of this game experience didn't have mobile support
-        we have since created a new mobile friendly build and uploaded
-        for you here:
-      </p>
+        <p>
+          This project was created right before web browsers
+          were upgraded with some very specific technologies that
+          would allow good performance on mobile browsers. The initial
+          version of this game experience didn't have mobile support
+          we have since created a new mobile friendly build and uploaded
+          for you here:
+        </p>
 
-      <p>
-        Working on this project I learned about browser support
-        for mobile gaming and realized we were right on the cusp.
-        The very idea for Tensor's specific offering of mobile first
-        web experience was born out of the realizations brought to light
-        by this project.
-      </p>
+        <p>
+          Working on this project I learned about browser support
+          for mobile gaming and realized we were right on the cusp.
+          The very idea for Tensor's specific offering of mobile first
+          web experience was born out of the realizations brought to light
+          by this project.
+        </p>
 
-      <img src="http://lorempixel.com/400/200" />
+        <img src="http://lorempixel.com/400/200" />
 
+      </div>
     </div>
   `,
   two: `
@@ -45,29 +49,33 @@ const articles = {
     </div>
 
     <div class="modal-article">
+      <div class="container">
 
-      <a class="button" target="_blank" href="https://aws-0003-viz.firebaseapp.com">PLAY NOW!</a>
+        <div style="text-align:center;margin: 20px;">
+          <a class="button" target="_blank" href="https://aws-0003-viz.firebaseapp.com">PLAY NOW!</a>
+        </div>
 
-      <p>
-        This was less of a game and more of an experimental experience.
-        This project was also for trainging purposes, but instead of presenting
-        business partners with a boring list we distributed the contents of data
-        in 3D space and allowed the viewers to pan around and explore the content
-        via floating geometries we dubbed "geods".
-      </p>
+        <p>
+          This was less of a game and more of an experimental experience.
+          This project was also for trainging purposes, but instead of presenting
+          business partners with a boring list we distributed the contents of data
+          in 3D space and allowed the viewers to pan around and explore the content
+          via floating geometries we dubbed "geods".
+        </p>
 
-      <img src="http://lorempixel.com/400/200?fuck=off" />
+        <img src="http://lorempixel.com/400/200?fuck=off" />
 
-      <p>
-        This project was initially setup to run on a very specific
-        resolution large touch screen. This was ideal since the deployment
-        was contained to physical events and we had access to the devices it ran on.
-        We have since uploaded a new version that is better suited to be tested
-        on all devices (including mobile). You can check that out here:
-      </p>
+        <p>
+          This project was initially setup to run on a very specific
+          resolution large touch screen. This was ideal since the deployment
+          was contained to physical events and we had access to the devices it ran on.
+          We have since uploaded a new version that is better suited to be tested
+          on all devices (including mobile). You can check that out here:
+        </p>
 
-      <img src="http://lorempixel.com/400/200" />
+        <img src="http://lorempixel.com/400/200" />
 
+      </div>
     </div>
   `,
   three: `
@@ -76,26 +84,30 @@ const articles = {
     </div>
 
     <div class="modal-article">
+      <div class="container">
 
-      <a class="button" target="_blank" href="https://starmaster.surge.sh">PLAY NOW!</a>
+        <div style="text-align:center;margin: 20px;">
+          <a class="button" target="_blank" href="https://starmaster.surge.sh">PLAY NOW!</a>
+        </div>
 
-      <p>
-        This is a proof of concept turned into a fully realized game. Tensor took
-        everything we learned from passed projects about making mobile games performant, playable,
-        and fun on the web and poored that expertise into this game.
-      </p>
-  
-      <p> 
-        Ever since the death of Adobe Flash web based games have also died.
-        But there is new technology that has recently been implemented by every major browser!
-        Not only is this new technology a replacement for flash based games.
-        It is far better and Star Master can prove it!
-      </p>
+        <p>
+          This is a proof of concept turned into a fully realized game. Tensor took
+          everything we learned from passed projects about making mobile games performant, playable,
+          and fun on the web and poored that expertise into this game.
+        </p>
+    
+        <p> 
+          Ever since the death of Adobe Flash web based games have also died.
+          But there is new technology that has recently been implemented by every major browser!
+          Not only is this new technology a replacement for flash based games.
+          It is far better and Star Master can prove it!
+        </p>
 
-      <img src="http://lorempixel.com/400/200?fuck=off" />
+        <img src="http://lorempixel.com/400/200?fuck=off" />
 
-      <img src="http://lorempixel.com/400/200" />
+        <img src="http://lorempixel.com/400/200" />
 
+      </div>
     </div>
   `,
   contact: `
@@ -104,26 +116,29 @@ const articles = {
     </div>
 
     <div class="modal-article">
-      <ul class="contact-list">
-        <li>
-          <i class="fa fa-calendar"></i><a href="https://calendly.com/tylerbuchea">Schedule a meeting</a>
-        </li>
-        <li>
-          <i class="fa fa-twitter"></i><a href="https://twitter.com/tylerbuchea" target="_blank">Tweet at us</a>
-        </li>
-        <li>
-          <i class="fa fa-envelope"></i><a href="mailto:tyler@buchea.com" target="_blank">Send us an email</a>
-        </li>
-        <li>
-          <form action="//formspree.io/tyler@buchea.com" method="post">
-            <input name="_replyto" placeholder="your-email@example.com" style="font-size: 12.5px;" type="email" />
-            <input class="button" type="submit" value="SEND" />
-          </form>
-        </li>
-      </ul>
-      
-      <!-- <img src="img/crew.gif" /> -->
+      <div class="container">
 
+        <ul class="contact-list">
+          <li>
+            <i class="fa fa-calendar"></i><a href="https://calendly.com/tylerbuchea">Schedule a meeting</a>
+          </li>
+          <li>
+            <i class="fa fa-twitter"></i><a href="https://twitter.com/tylerbuchea" target="_blank">Tweet at us</a>
+          </li>
+          <li>
+            <i class="fa fa-envelope"></i><a href="mailto:tyler@buchea.com" target="_blank">Send us an email</a>
+          </li>
+          <li>
+            <form action="//formspree.io/tyler@buchea.com" method="post">
+              <input name="_replyto" placeholder="your-email@example.com" style="font-size: 12.5px;" type="email" />
+              <input class="button" type="submit" value="SEND" />
+            </form>
+          </li>
+        </ul>
+        
+        <!-- <img src="img/crew.gif" /> -->
+
+      </div>
     </div>
   `,
 };
